8e411a2852022a25e13b835c6a1092db023d646e
Dan MacLean <maclean.daniel@gmail.com>
2014-08-22 15:52:12 +0100

    replace run.py

--- bin/run.py
+++ bin/run.py
@@ -13,50 +13,90 @@ import random
 import argparse
 from Bio import SeqIO
 from subprocess import call
+import runners
 
-parser = argparse.ArgumentParser(description='Make datastructures of kmer overlaps from sets of genes. Save them for loading back in later')
-parser.add_argument("-k", "--kmer_length", type=int, help="length of k to use", default=31)
-parser.add_argument("-f", "--fasta", type=str, help="FASTA sequences to load")
-parser.add_argument("-n", "--sample_sizes", nargs='+', type=int, help="list of number of genes to select")
-parser.add_argument("-i", "--bootstraps", type=int, help="number of bootstrap iterations to select")
-opts = parser.parse_args()
+#parser = argparse.ArgumentParser(description='Make datastructures of kmer overlaps from sets of genes. Save them for loading back in later')
+#parser.add_argument("-k", "--kmer_length", type=int, help="length of k to use", default=31)
+#parser.add_argument("-f", "--fasta", type=str, help="FASTA sequences to load")
+#parser.add_argument("-n", "--sample_sizes", nargs='+', type=int, help="list of number of genes to select")
+#parser.add_argument("-i", "--bootstraps", type=int, help="number of bootstrap iterations to select")
+#opts = parser.parse_args()
 
-def make_graph(kmer_info):
-	g = nx.Graph()
-	for i in kmer_info.kmer_attrs:
-		g.add_node(i, kmer_info[i])
-	g.add_edges_from(kmer_info.edges)
-	return g
+make_reads = True
+LARGEST_PARTITION_LIMIT = 0.5 #max proportion of reads allowed in any partition - a parameter for stopping iterative partitioning
 
-def select_from_fasta(fasta, sample_size, iteration):
-	seq_list = list(SeqIO.parse(fasta, "fasta"))
-	filename = "_".join([str(sample_size), str(iteration),fasta ] ) 
-	indices = random.sample(range(len(seq_list)), sample_size)
-	try:
-	 	write_fasta( [seq_list[i] for i in indices], filename )
-	except IOError as e:
-		print "I/O error({0}): {1}".format(e.errno, e.strerror)
-		sys.exit
-	return filename
+def do_a_partition():
+	runners.load_graph()
+	runners.partition_graph()
+	runners.merge_partition()
+	runners.annotate_partitions()
+	runners.extract_partitions()
+	clean_up()
+	
+def break_a_lump():
+	pass
 
+def count_partition_distributions():
+	prog = re.compile('^>(.*)\s+(\d+)\n')
+	parts = {}
+	seqs = []
+	with open(in_file, "r") as f:
+		for line in f.readlines():
+			m = prog.match(line)
+			if m:
+				seqs.append(m.group(1))
+				if int(m.group(2)) in parts:
+					parts[int(m.group(2))].append(m.group(1))
+				else:
+					parts[int(m.group(2))] = [m.group(1)]
+				return max(parts.keys()), list(set(seqs)), parts
 
-def write_fasta(seq_list, filename):
-	output_handle = open(filename, "w")
-	count = SeqIO.write(seq_list, output_handle, "fasta")
-	output_handle.close()
-	return True
+def clean_up():
+	pass
 
-def run_job(fasta,kmer_length):
-	print "running job with file {0} and k {1}".format(fasta,kmer_length)
-	call(["bsub", "-q TSL-Test128", "python", "worker.py", fasta, str(kmer_length)])
+def break_lump(lump):
+	runners.load_graph()
+	runners.make_initial_stop_tags()
+	
+	runners.partition_graphs()
+	runners.filter_stoptags()
 
-def main(opts):
-	for sample_size in opts.sample_sizes:
-		mini_fasta_file_names = [select_from_fasta(opts.fasta,sample_size, i) for i in range(opts.bootstraps) ]
-		for fasta in mini_fasta_file_names:
-			run_job(fasta,opts.kmer_length)
+def assemble():
+	pass	
 
+def break_lumps(lump_list):
+	for lump in lump_list:
+		break_lump(lump)
+		do_a_partition()
+		count_partition_distributions()
+
+def main():
+	if make_reads:
+		runners.make_reads()
+	##check state of assembly prior
+	runners.velvet()
+	runners.summarise_assembly()
+	##count kmers prior
+	runners.load_into_counting(k=31,n=4, b=2e8, t=1, outfile='meh', infile="../data/3_rgenes.fna", )
+	runners.abundance_dist()
+	##normalize
+	runners.normalize_by_median()
+	##count normalised kmers
+	runners.load_into_counting()
+	runners.abundance_dist()
+	
+	
+	largest_partition = 1.0 
+	do_a_partition()
+	largest_partition = count_partition_distributions()
+	##remove knots iteratively
+	while LARGEST_PARTITION_LIMIT < largest_partition:
+		break_lumps()
+		
+
+	
+	
 
 
 if __name__ == "__main__":
-	main(opts)
+	main()
